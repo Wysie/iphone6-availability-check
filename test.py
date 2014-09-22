a86ebc428c47ce21d1f10bdd23385cfbe95a51a7
Soh Yuan Chin <sohyuanchin@gmail.com>
2014-09-22 09:19:59 +0800

    Extract raw JSON data to a list of dictionaries.

--- test.py
+++ test.py
@@ -5,20 +5,41 @@ from bs4 import BeautifulSoup
 
 page = urllib2.urlopen("http://store.apple.com/sg/buy-iphone/iphone6")
 soup = BeautifulSoup(page)
-
 script = soup.find('script', text=re.compile('window\.productSelectionController\.addData'))
 json_text = re.search(r'^\s*window\.productSelectionController\.addData\(({.*?})\s*\);\s*$', script.string, flags=re.DOTALL | re.MULTILINE).group(1)
 data = json.loads(json_text)
 
-for d in data["products"]:
-	if d["dimensionScreensize"] == "4_7inch":
-		print "Model: iPhone 6"
-	elif d["dimensionScreensize"] == "5_5inch":
-		print "Model: iPhone 6 Plus"
-	else:
-		print "Model: iDiot"
+data_extract = []
 
-	print "Colour: {0}".format(d["dimensionColor"].capitalize())
-	print "Capacity: {0}".format(d["dimensionCapacity"].capitalize())
-	print "Price: ${0}".format(d["price"].replace("_", "."))
-	print "Availability: {0}\n".format(d["displayShippingQuote"].capitalize())
+for d in data["products"]:
+	model = "iDiot"
+	if d["dimensionScreensize"] == "4_7inch":
+		model = "iPhone 6" 
+	elif d["dimensionScreensize"] == "5_5inch":
+		model =  "iPhone 6 Plus"
+
+	colour = d["dimensionColor"].replace("_", " ").title()
+	capacity = d["dimensionCapacity"].title()
+	price = d["price"].replace("_", ".")
+	shipping_quote = d["displayShippingQuote"].title()
+	availablility = "No" if shipping_quote == "Currently Unavailable" else "Yes"
+		
+	print "Model: {0}".format(model)
+	print "Colour: {0}".format(colour)
+	print "Capacity: {0}".format(capacity)
+	print "Price: ${0}".format(price)
+	print "Shipping Quote: {0}".format(shipping_quote)
+	print "Availablility: {0}\n".format(availablility)
+
+	info = {
+		'model': model,
+		'colour': colour,
+		'capacity': capacity,
+		'price': price,
+		'shippingQuote': shipping_quote,
+		'availablility': availablility
+	}
+
+	data_extract.append(info)
+
+print data_extract
